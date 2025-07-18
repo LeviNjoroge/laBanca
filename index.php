1737e868ed18eee8610593f0a5d6f1d4ae615ed8
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-18 18:08:38 +0300

    preping labanca

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ include("api.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Demo</title>
+    <title>LaBanca</title>
 </head>
 <body>
     <h1>Hi <?php echo $first_name?>!</h1>
