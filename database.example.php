4ee6caa572545f41e1c780468cf25aefc0bba906
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-18 18:12:30 +0300

    preping labanca

--- database.example.php
+++ database.example.php
@@ -3,13 +3,14 @@
 
 //
 
-$db_server = "YOUR_SERVER_NAME_HERE";
 $db_host = "YOUR_HOST_NAME_HERE";
+$db_user = "YOUR_USER_NAME_HERE";
 $db_pass = "YOUR_PASSWORD_HERE";
 $db_name = "YOUR_DATABASE_NAME_HERE";
 
 try{
-    $conn = mysqli_connect($db_server, $db_host, $db_pass, $db_name);
+    $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+    mysqli_conn
 }
 catch(Exception $e){
     echo "<h1>Warning! Could not connect to database!</h1><br>";
