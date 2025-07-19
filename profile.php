1dbd1f59ffa570d0e6bf817f2f3673ff231d54f0
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-19 17:35:25 +0300

    Working on labanca

---
+++ profile.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+include("api.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LaBanca: Profile</title>
+</head>
+<body>
+    
+
+<!--Logout button-->
+    <form action="" method="post"><input type="submit" value="logout" name="logout" id="logout"></form>
+</body>
+</html>
