b82a0c3ca13f9dcba7fc9888db30646db4231b78
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-19 13:57:56 +0300

    Working on labanca

--- signup.php
+++ signup.php
@@ -8,40 +8,41 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <title>LaBanca</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <div class="form-container">
         <h1>Welcome to LaBanca!</h1>
         <h2>Registration Form</h2>
         <form action="" method="post">
-            <label for="first_name">First Name:</label>
-            <input type="text" name="first_name" id="first_name" placeholder="Joe" required>
+            <label for="first_name">First Name:</label> 
+            <input type="text" name="first_name" id="first_name" placeholder="Joe" required> <br>
 
-            <label for="last_name">Last Name:</label>
-            <input type="text" name="last_name" id="last_name" placeholder="William">
+            <label for="last_name">Last Name:</label> 
+            <input type="text" name="last_name" id="last_name" placeholder="William"> <br>  
 
             <label for="surname">Surname:</label>
-            <input type="text" name="surname" id="surname" placeholder="Doe" required>
+            <input type="text" name="surname" id="surname" placeholder="Doe" required> <br><hr>
 
-            <label for="date_of_birth">Date of Birth:</label>
-            <input type="date" name="date_of_birth" id="date_of_birth" required>
+            <label for="date_of_birth">Date of Birth:</label> 
+            <input type="date" name="date_of_birth" id="date_of_birth" required> <br>
 
-            <label for="id">National ID No.:</label>
-            <input type="number" name="id" id="id" placeholder="88888888" required>
+            <label for="id">National ID No.:</label> 
+            <input type="number" name="id" id="id" placeholder="88888888" required> <br> <hr>
 
-            <label for="username">Username:</label>
-            <input type="text" name="username" id="username" placeholder="joedoe01" required>
+            <label for="username">Username:</label> 
+            <input type="text" name="username" id="username" placeholder="joedoe01" required> <br>
 
-            <label for="email">Email Address:</label>
-            <input type="email" name="email" id="email" placeholder="example@email.com">
+            <label for="email">Email Address:</label> 
+            <input type="email" name="email" id="email" placeholder="example@email.com"> <br>
 
-            <label for="phone">Phone Number:</label>
-            <input type="tel" name="phone" id="phone" placeholder="0712345678">
+            <label for="phone">Phone Number:</label> 
+            <input type="tel" name="phone" id="phone" placeholder="0712345678"> <br>
 
-            <label for="password">Password:</label>
-            <input type="password" name="password" id="password" required>
+            <label for="password">Password:</label> 
+            <input type="password" name="password" id="password" required> <br>
 
-            <input type="submit" value="Submit" name="signup">
+            <input type="submit" value="Submit" name="signup" id="submit"> <br>
         </form>
     </div>
 </body>
