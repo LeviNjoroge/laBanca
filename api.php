bb09f7b0f8510c3d7b4769ce66c102dff72e9b2f
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-23 22:52:16 +0300

    working in labanca

--- api.php
+++ api.php
@@ -3,18 +3,20 @@
 
 include("database.php");
 
+// check user's balance
 function check_balance($id){
     $sql_check_balance = "SELECT balance FROM users WHERE id = {$id}";
     $result = mysqli_query($GLOBALS["conn"], $sql_check_balance);
     if (mysqli_num_rows($result)>0) {
         $row = mysqli_fetch_assoc($result);
-        return $row["balance"] ?? 0;
+        return $row["balance"];
     }
     else{
-        return "0.00";
+        return 0;
     }
 }
 
+// user can deposit some amount
 function deposit_cash($user_id, $amount){
     try {
         $query_update_deposited_amount = "UPDATE users SET balance = balance + {$amount} WHERE id = {$user_id}";
@@ -22,10 +24,18 @@ function deposit_cash($user_id, $amount){
 
         $query_record_deposit = "INSERT INTO transactions(user_id, message) VALUES('{$user_id}','DEPOSITED \${$amount}')";
         mysqli_query($GLOBALS['conn'], $query_record_deposit);
-        
+
         echo "<script>alert('You have successfully deposited \${$amount}')</script>";
     } catch (Exception $e) {
         echo "<script>alert('Was unable to deposit you money:(')</script>";
-        throw $e;
+    }
+}
+
+// user can transfer cash to another user
+function transfer_cash($from, $to, $amount){
+    try {
+        
+    } catch (\Throwable $th) {
+        //throw $th;
     }
 }
