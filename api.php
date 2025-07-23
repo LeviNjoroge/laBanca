7c36003878ca298c8abda6d900fafc921447cdcf
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-23 22:44:44 +0300

    working in labanca

--- api.php
+++ api.php
@@ -19,8 +19,10 @@ function deposit_cash($user_id, $amount){
     try {
         $query_update_deposited_amount = "UPDATE users SET balance = balance + {$amount} WHERE id = {$user_id}";
         mysqli_query($GLOBALS['conn'], $query_update_deposited_amount);
+
         $query_record_deposit = "INSERT INTO transactions(user_id, message) VALUES('{$user_id}','DEPOSITED \${$amount}')";
         mysqli_query($GLOBALS['conn'], $query_record_deposit);
+        
         echo "<script>alert('You have successfully deposited \${$amount}')</script>";
     } catch (Exception $e) {
         echo "<script>alert('Was unable to deposit you money:(')</script>";
