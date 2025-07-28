fa6eb85cc603a55708f23ecfead624078da82900
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-28 14:18:40 +0300

    working on labanca

--- contact.php
+++ contact.php
@@ -11,11 +11,11 @@ include("components/header.php");
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <h2>Contact Us</h2>
+    <h2 style="font-size: 2rem;">Contact Us</h2>
     <div class="contact-form">
         <label for="message">Leave a review, comment, or query:</label> <br>
         <textarea name="message" id="message" placeholder="Write your message here..."></textarea> <br>
-        <input type="submit" value="submit" name="submit-comment"> <br>
+        <input type="submit" value="Submit" name="submit-comment" class="submit-comment"> <br>
     </div>
 <?php
 include("components/footer.php");
