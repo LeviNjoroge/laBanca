8da4105f376a9112a4ad31c11c973afb37ee0bb4
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-29 23:53:05 +0300

    working on labanca

--- contact.php
+++ contact.php
@@ -19,10 +19,10 @@ include("components/header.php");
             <input type="submit" value="Submit" name="submit-comment" class="submit-comment"> <br>
         </div>
         <div class="display_contacts">
-            <h3>Reach us via</h3>
-            <p>Phone: +254712345678</p>
-            <p>Email: support@labanca.com</p>
-            <p>Address: 123 Main Street, Nairobi</p>
+            <h3>Reach us via...</h3>
+            <p>Phone: <a href="tel:+254712345678" style="color: black; text-decoration: none;">+254712345678</a></p>
+            <p>Email: <a href="mailto:support@labanca.com" style="color: black; text-decoration: none;">support@labanca.com</a></p>
+            <p>Address: 123 Main Street, Rome, Italy</p>
         </div>
     </div>
 
