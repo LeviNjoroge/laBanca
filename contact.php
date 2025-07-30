aaedfcaf8d39682a9bf7621b4838cfc51da459fb
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-30 22:56:54 +0300

    working on labanca

--- contact.php
+++ contact.php
@@ -16,12 +16,12 @@ include("components/header.php");
         <h2 style="font-size: 2rem;">Contact Us</h2>
         <div class="contact-form">
             <label for="tel">Telephone Number:</label>
-            <input type="tel" name="tel" id="tel" value="<?php echo $_SESSION['phone']??'N/A'?>" style="width:min-content;" size="13"> <br>
-            <label for="tel">Email Address:</label>
-            <input type="email" name="email" id="email" value="<?php echo $_SESSION['email']??'N/A'?>" style="width:min-content;" size="13"> <br>
+            <input id="tel" type="tel" name="tel" id="tel" value="<?php echo $_SESSION['phone']??'N/A'?>" style="width:min-content;" size="13"> <br>
+            <label for="email">Email Address:</label>
+            <input id="email" type="email" name="email" id="email" value="<?php echo $_SESSION['email']??'N/A'?>" style="width:min-content;"> <br>
             <label for="message">Leave a review, comment, or query:</label> <br>
             <textarea name="message" id="message" placeholder="Write your message here..."></textarea> <br>
-            <input type="submit" value="Submit" name="submit-comment" class="submit-comment"> <br>
+            <input type="submit" value="Submit" name="submit-comment" class="submit-comment" onsubmit="submitReview()"> <br>
         </div>
         <div class="display_contacts">
             <h3>Reach us via...</h3>
