b991fc2172181d5d4dedaffc37e478e078584020
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-30 23:07:25 +0300

    working on labanca

--- script.js
+++ script.js
@@ -2,4 +2,12 @@ function submitReview(params) {
     var tel = document.getElementById('tel');
     var email = document.getElementById('email');
     var message = document.getElementById('message');
+    var time = now();
+
+    var data = {
+        tel: tel.value,
+        email: email.value,
+        message: message.value,
+        time: time
+    };
 }
