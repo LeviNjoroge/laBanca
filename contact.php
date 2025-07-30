39ea711ef24f15f2edf93879e5fafe49becde21d
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-30 23:14:49 +0300

    working on labanca

--- contact.php
+++ contact.php
@@ -14,7 +14,8 @@ include("components/header.php");
 <body>
     <div class="content">
         <h2 style="font-size: 2rem;">Contact Us</h2>
-        <div class="contact-form">
+        <form action="" method="post">
+            <div class="contact-form">
             <label for="tel">Telephone Number:</label>
             <input id="tel" type="tel" name="tel" id="tel" value="<?php echo $_SESSION['phone']??'N/A'?>" style="width:min-content;" size="13"> <br>
             <label for="email">Email Address:</label>
@@ -23,6 +24,7 @@ include("components/header.php");
             <textarea name="message" id="message" placeholder="Write your message here..."></textarea> <br>
             <input type="submit" value="Submit" name="submit-comment" class="submit-comment" onsubmit="submitReview()"> <br>
         </div>
+        </form>
         <div class="display_contacts">
             <h3>Reach us via...</h3>
             <p>Phone: <a href="tel:+254712345678" style="color: black; text-decoration: none;">+254712345678</a></p>
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function submitReview(params) {
+function submitReview() {
     var tel = document.getElementById('tel');
     var email = document.getElementById('email');
     var message = document.getElementById('message');
@@ -11,5 +11,9 @@ function submitReview(params) {
         time: time
     };
 
+    // feed this to customer_feedback.json
+    var feedback = JSON.stringify(Feedback);
+    localStorage.setItem('customer_feedback', feedback);
     
+    alert('Thank you for your feedback!');
 }
