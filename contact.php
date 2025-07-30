d0a9564942a8126d058b880b04a92af0534feb79
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-30 22:48:34 +0300

    working on labanca

--- contact.php
+++ contact.php
@@ -8,6 +8,7 @@ include("components/header.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LaBanca: Contact Us</title>
+    <script src=""></script>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
