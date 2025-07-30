f738b3b0fb13e177c7be9b15488e5b9511a0858a
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-30 22:54:19 +0300

    working on labanca

--- contact.php
+++ contact.php
@@ -18,7 +18,7 @@ include("components/header.php");
             <label for="tel">Telephone Number:</label>
             <input type="tel" name="tel" id="tel" value="<?php echo $_SESSION['phone']??'N/A'?>" style="width:min-content;" size="13"> <br>
             <label for="tel">Email Address:</label>
-            <input type="email" name="email" id="email" value="<?php echo $_SESSION['phone']??'N/A'?>" style="width:min-content;" size="13"> <br>
+            <input type="email" name="email" id="email" value="<?php echo $_SESSION['email']??'N/A'?>" style="width:min-content;" size="13"> <br>
             <label for="message">Leave a review, comment, or query:</label> <br>
             <textarea name="message" id="message" placeholder="Write your message here..."></textarea> <br>
             <input type="submit" value="Submit" name="submit-comment" class="submit-comment"> <br>
