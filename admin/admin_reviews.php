6d5ccfde5205e11f7c8c0f53a6755923e5f75493
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-31 21:38:41 +0300

    working on labanca

--- admin/admin_reviews.php
+++ admin/admin_reviews.php
@@ -8,7 +8,7 @@ include("api.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>laBanca Users</title>
+    <title>laBanca Reviews</title>
     <link rel="stylesheet" href="/styles.css">
 </head>
 <body>
