9d66d10c863f7e89f8771d65b0860c563d69cd48
Levi Njoroge <Levynjoroge1@gmail.com>
2025-07-31 21:30:10 +0300

    working on labanca

--- admin/admin.php
+++ admin/admin.php
@@ -3,3 +3,11 @@ session_start();
 include("api.php");
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LaBanca</title>
+    <link rel="stylesheet" href="/styles.css">
+</head>
