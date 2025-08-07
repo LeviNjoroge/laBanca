c9eefb272f87229287aca48a14613cbeaa574b0a
Levi Njoroge <Levynjoroge1@gmail.com>
2025-08-07 16:38:23 +0300

    working on labanca

--- index.php
+++ index.php
@@ -5,9 +5,11 @@ This page will host the following:
     their current balance
     recent transactons
 */
+
 // if(isset($_COOKIE['user_id'])){
 //     $_SESSION['id'] = $_COOKIE['user_id'];
 // }
+
 include("components/header.php");
 $first_name= $_SESSION['first_name'];
 $last_name= $_SESSION['last_name'];
