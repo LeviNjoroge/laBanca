e32d06b838bccf10d3a3f5daf6ad004add5ac2cd
Levi Njoroge <Levynjoroge1@gmail.com>
2025-09-06 19:57:53 +0300

    working on labanca: oauth updates

--- gClientSetup.php
+++ gClientSetup.php
@@ -1,5 +1,9 @@
 <?php
 
-require __DIR__ . "../vendor/"
+require __DIR__ . "../vendor/autoload.php";
+
+$client = new Google\Client();
+$client->setClientId("731434090589-29ggu4sl4cc7b1necf7s1o00ocluj3ij.apps.googleusercontent.com");
+$client->setClientSecret("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");
 
 ?>
