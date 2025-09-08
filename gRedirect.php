2dae6c3a909abd77c5e55cbb8f8d236e59b10d4b
Levi Njoroge <Levynjoroge1@gmail.com>
2025-09-08 16:38:33 +0300

    working on labanca: oauth updates

--- gRedirect.php
+++ gRedirect.php
@@ -6,7 +6,8 @@ if(!isset($_GET["code"])){
     echo "<script>alert('Login Failed, please retry another time')</script>";
 }
 
-$token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+try {
+    $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
 $client->setAccessToken($token['access_token']);
 
 $oauth = new Google\Service\Oauth2($client);
@@ -14,5 +15,8 @@ $oauth = new Google\Service\Oauth2($client);
 $userInfo = $oauth->userinfo->get();
 echo "<h1>Hello $userInfo->name</h2>";
 echo $userInfo ->profile;
+} catch (\Throwable $th) {
+    header("Location: signin.php");
+}
 
 ?>
