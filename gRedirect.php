0079ee634623fd7c8b3498fc80cb1646cf9d9ce9
Levi Njoroge <Levynjoroge1@gmail.com>
2025-09-08 17:03:11 +0300

    working on labanca: oauth updates

--- gRedirect.php
+++ gRedirect.php
@@ -1,6 +1,7 @@
 <?php
 
 require "gClientSetup.php";
+require "database.php";
 
 if(!isset($_GET["code"])){
     echo "<script>alert('Login Failed, please retry another time')</script>";
@@ -22,9 +23,27 @@ try {
 
     // database logs
     $name = explode($userInfo["name"]," ");
-    $firstname = $name[0];
+    $first_name = $name[0];
     $surname = $name[1]??"";
-    $lastname = $name[2]??"";
+    $last_name = $name[2]??"";
+    $id = $userInfo["id"];
+    $email_address = $userInfo["email"];
+    $sql_add_user = "
+                        INSERT INTO users(id, first_name, last_name, surname, email_address)
+                        VALUES ('$id','$first_name', '$last_name', '$surname', '$email')
+    ";
+    try{
+        mysqli_query($conn, $sql_add_user);
+        header("Location: index.php");
+    }
+    catch(Exception $e){
+        if (str_contains($e->getMessage(), 'Duplicate entry')) {
+        $error = "Duplicate entry!";
+        }
+        else {
+            $error = "Could not register user. <br>Try again later!";
+        }
+    }
 
 } catch (\Throwable $th) {
     header("Location: signin.php");
