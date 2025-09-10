593d21c22585395357f6263c884a6c566a7ece1a
Levi Njoroge <Levynjoroge1@gmail.com>
2025-09-10 12:18:20 +0300

    working on labanca

---
+++ currencies.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Currency exchanges</title>
+</head>
+<body>
+    <h1>Currency Exchanges</h1>
+    <p>Below are the currency pair trading values against USD</p>
+</body>
+</html>
