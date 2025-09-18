7a701b469dd7f94742f3a32371d615ec97294ff5
Levi Njoroge <Levynjoroge1@gmail.com>
2025-09-18 16:28:02 +0300

    including USSD in laBanca

--- USSD/functions.php
+++ USSD/functions.php
@@ -3,7 +3,7 @@ include "../database.php";
 function checkRegistration($user_username){
     $verify_user = "SELECT * FROM users WHERE username = '$user_username' OR email_address = '$user_username' OR id='$user_username";
 
-    $result = mysqli_query($conn, $verify_user);
+    $result = mysqli_query($GLOBALS["conn"], $verify_user);
 
     if (mysqli_num_rows($result) > 0) {
         $user = mysqli_fetch_assoc($result);
