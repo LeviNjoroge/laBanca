68e6f1ef9383b3f8097a7fc78a15adb1ba0fa61c
Levi Njoroge <Levynjoroge1@gmail.com>
2025-09-18 15:14:28 +0300

    including USSD in laBanca

--- USSD.php
+++ USSD.php
@@ -8,29 +8,18 @@ $serviceCode = $_POST["serviceCode"];
 $phoneNumber = $_POST["phoneNumber"];
 $text        = $_POST["text"];
 
-if ($text == "") {
-    // This is the first request. Note how we start the response with CON
-    $response  = "CON Welcome to laBanca \n";
-    $response .= "1. Register for an account\n";
-    $response .= "2. Login";
-
-} else if ($text == "1") {
-    // Business logic for first level response
-    $response = "CON Choose account information you want to view \n";
-    $response .= "1. Account number \n";
-
-} else if ($text == "2") {
-    // Business logic for first level response
-    // This is a terminal request. Note how we start the response with END
-    $response = "END Your phone number is ".$phoneNumber;
-
-} else if($text == "1*1") { 
-    // This is a second level response where the user selected 1 in the first instance
-    $accountNumber  = "ACC1001";
-
-    // This is a terminal request. Note how we start the response with END
-    $response = "END Your account number is ".$accountNumber;
-
+switch($text){
+    case "":
+        $response  = "
+        CON Welcome to laBanca! \n
+        1. Register for an account \n
+        2. Log into an existing account \n
+        ";
+        break;
+    case "1":
+        $response = "
+        END Please visit www.labanca.com/register to register for an account \n
+        ";
 }
 
 // Echo the response back to the API
