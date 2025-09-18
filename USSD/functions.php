be2c8df8ad9c6124922fefd58aad6c113c771976
Levi Njoroge <Levynjoroge1@gmail.com>
2025-09-18 20:23:49 +0300

    working on USSD for laBanca

--- USSD/functions.php
+++ USSD/functions.php
@@ -1,16 +1,26 @@
 <?php
 include "../database.php";
 function checkRegistration($user_username){
-    $verify_user = "SELECT * FROM users WHERE username = '$user_username' OR email_address = '$user_username' OR id='$user_username";
-
+    $verify_user = "SELECT * FROM users WHERE username = '$user_username' OR email_address = '$user_username' OR id='$user_username'";
     $result = mysqli_query($GLOBALS["conn"], $verify_user);
 
     if (mysqli_num_rows($result) > 0) {
         $user = mysqli_fetch_assoc($result);
-        return $user;
+        return $user['id'];
     }
     else{
         return 0;
     }
 }
 
+function verifyUser($user_username, $user_password){
+    $verify_user = "SELECT password FROM users WHERE username = '$user_username' OR email_address = '$user_username' OR id='$user_username'";
+    $result = mysqli_query($GLOBALS["conn"], $verify_user);
+    $row = mysqli_fetch_assoc($result);
+    $hashedPassword = $row["password"];
+    if (password_verify($user_password, $hashedPassword)) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
